c3f1eb6f27d63835e9b623d0990ecce17d1bbb6c
reham sherif <rehamsherrif@gmail.com>
2026-01-06 21:46:37 +0200

    Edited gh page

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-  base: '/E-commerce-website/',   
+  plugins: [react()],
+  base: '/E-commerce-website/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
+  }
 })
