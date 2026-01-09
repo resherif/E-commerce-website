7cce8b3c6b8b843962383f0084f5d872c3746062
reham sherif <rehamsherrif@gmail.com>
2026-01-09 15:39:30 +0200

    package

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: '/E-commerce-website/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-   
-  },
-    publicDir: 'public', 
+  publicDir: 'public',
 })
